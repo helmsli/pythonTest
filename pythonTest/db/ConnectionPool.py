269e35388930c5cb3940655c811d8907a78dd13c
helmsli <helmsli@163.com>
2017-05-29 22:13:19 +0800

    init

---
+++ pythonTest/db/ConnectionPool.py
@@ -0,0 +1,45 @@
+﻿# -*- coding: UTF-8 -*-
+"""
+@描述：数据库连接池管理模块
+@作者：CYH
+@版本：V1.0
+@创建时间：2016-11-24 上午8:43:14
+"""
+
+import pymysql;
+from DBUtils.PooledDB import PooledDB;
+
+import DbCon as Config;
+
+'''
+@功能：PT数据库连接池
+'''   
+class PTConnectionPool(object):
+	__pool = None;
+    
+	def getConn(self):
+		if self.__pool is None:
+			self.__pool = PooledDB(creator=pymysql, mincached=Config.DB_MIN_CACHED , maxcached=Config.DB_MAX_CACHED, 
+                                   maxshared=Config.DB_MAX_SHARED, maxconnections=Config.DB_MAX_CONNECYIONS, 
+                                   blocking=Config.DB_BLOCKING, maxusage=Config.DB_MAX_USAGE, 
+                                   setsession=Config.DB_SET_SESSION,
+                                   host=Config.DB_HOST , port=Config.DB_PORT , 
+                                   user=Config.DB_USER , passwd=Config.DB_PASSWORD ,
+                                   db=Config.DB_DBNAME , use_unicode=False, charset=Config.DB_CHARSET);
+
+		return self.__pool.connection();
+		
+	
+	def __enter__(self):
+		self.conn = self.getConn();
+		self.cursor = self.conn.cursor();
+		print "create con and cursor";
+		return self;
+	
+	def __exit__(self,type,value,trace):
+		self.cursor.close();
+		self.conn.close();
+		print "close con and cursor";
+		 
+def getPTConnection():
+	return PTConnectionPool();
---
+++ pythonTest/db/DbTest.py
@@ -0,0 +1,31 @@
+﻿#-*- coding: UTF-8 -*-  
+'''
+@描述：
+@作者：CYH
+@版本：V1.0
+@创建时间：2016-11-24 上午9:34:54
+'''
+from ConnectionPool import getPTConnection,PTConnectionPool;
+
+def TestMySQL():  
+    #申请资源  
+    with getPTConnection() as db:
+        # SQL 查询语句;
+        sql = "SELECT * FROM tb_common_biz";
+        try:
+            # 获取所有记录列表
+            db.cursor.execute(sql)
+            results = db.cursor.fetchall();
+            for row in results:
+                userId = row[0]
+                name= row[1]         
+                sex= row[3]
+                createTime = row[4]
+                # 打印结果
+                print ("userId=%s,name=%s,sex=%s,createTime=%s" %\
+                     (userId, name, sex, createTime ))
+        except Exception,e: 
+            print ("Error: unable to fecth data")
+            print Exception,":",e
+
+TestMySQL()
