230314b6034c84dd47344273f8785e8d23c573a7
helmsli <helmsli@163.com>
2017-05-29 22:56:26 +0800

    add json example

---
+++ pythonTest/json/TestJson.py
@@ -0,0 +1,6 @@
+﻿#-*- coding: UTF-8 -*-  
+import json
+jsonStr = json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+print(jsonStr);
+jsonObj = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
+print(jsonObj);
