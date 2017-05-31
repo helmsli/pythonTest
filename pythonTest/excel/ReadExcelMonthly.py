db20c6d36ef4d809fb5a9bb689687a0df4e155c0
helmsli <helmsli@163.com>
2017-05-31 18:27:30 +0800

    test monthly success

--- pythonTest/excel/ReadExcelMonthly.py
+++ pythonTest/excel/ReadExcelMonthly.py
@@ -6,17 +6,34 @@ from datetime import date,datetime
 
 def IsMergeCol(colIndex,mergeCol):
 	for(rlow,rhigh,clow,chigh) in mergeCol:
+		
 		if((colIndex >= clow and colIndex < chigh)):
+			print "(((((((((((((((((((((((((((((((((((((((((("
 			return [True,rlow,rhigh,clow,chigh];
-		else:
-			return [False];
 			
-def IsMergeRow(rowIndex,mergeRow):
+		
+	return [False];
+			
+def IsMergeRow(rowIndex,colIndex,mergeRow):
 	for(rlow,rhigh,clow,chigh) in mergeRow:
-		if((rowIndex >= rlow and rowIndex < rhigh)):
+		if((rowIndex >= rlow and rowIndex < rhigh) and (colIndex>=clow and colIndex< chigh)):
 			return [True,rlow,rhigh,clow,chigh];
-		else:
-			return [False];			
+	return [False];			
+def getSingleCellValue(colRows,colIndex):
+	return ["single",colRows,colIndex,sheet.cell_value(colRows,colIndex)]
+def getMergeCellValue(sRow,eRow,sCol,eCol):
+	tableData = []
+	for sRowIndex in range(sRow,eRow):
+		rowData=[]
+		for sColIndex in range(sCol,eCol):
+			if (sheet.cell(sRowIndex,sColIndex).ctype == 3):
+				date_value = xlrd.xldate_as_tuple(sheet.cell_value(sRowIndex,sColIndex),book.datemode)
+				date_tmp = date(*date_value[:3]).strftime('%Y/%m/%d')
+				rowData.append(date_tmp)
+			else:
+				rowData.append(sheet.cell_value(sRowIndex,sColIndex))
+		tableData.append(rowData);
+	return ["list",colRows,colIndex,tableData]
 
 xlsfile = r'/media/sf_vshare/python/pythonTest/excel/file/report.xls'
 book = xlrd.open_workbook(xlsfile,formatting_info=True)     #获得excel的book对象 
@@ -48,18 +65,48 @@ for (rlow,rhigh,clow,chigh) in sheet.merged_cells:
 
 print mergeRow,mergeCol
 print sheet.merged_cells
-#loop for mergeRow
-#loop merge rows
-for colRows in range(titleRowInfo+1,nrows):
-	for(rlow,rhigh,clow,chigh) in mergeRow:
-		#if the column in mergeCol
-		for colIndex in range(0,ncols):
+#loop for row
+sheet_data = {}
+sheet_data["header"]=title_data;
+sheet_data["data"]=[];
+colRows = titleRowInfo+1
+while(colRows<nrows):
+#for colRows in range(titleRowInfo+1,nrows):
+   #loop column for the row of 0 column
+	isMergeRow = IsMergeRow(colRows,0,mergeRow)
+	mergeStartRow = colRows
+	mergeEndRow = colRows
+	row_data=[]
+	#get the merge information
+	if(isMergeRow[0]):
+		mergeStartRow=isMergeRow[1];
+		mergeEndRow=isMergeRow[2]-1;
+	else:
+		colRows=colRows+1
+		continue
+	print "mergeEndRow",colRows,mergeEndRow,nrows
+	colIndex=0;
+	while(colIndex<ncols):
+	#for colIndex in range(0,ncols):
+		#get the merge row,if merge read
+		isMergeRow = IsMergeRow(colRows,colIndex,sheet.merged_cells)
+		print "ismergeRow:",colRows,colIndex,isMergeRow
+		if(isMergeRow[0]):
+			row_data.append(getSingleCellValue(colRows,colIndex));
+		#no row merge ,only column merge
+		else:
+			#if the col is the merge column
 			isMergeCol = IsMergeCol(colIndex,mergeCol)
-			#merge column 
+			#merge columns 
+			mergeStartCol = colIndex
+			mergeEndCol = colIndex
 			if(isMergeCol[0]):
-				
-			else:
-			
-			
-		
-			print isMergeCol
+				mergeStartCol=isMergeCol[3];
+				mergeEndCol=isMergeCol[4]-1
+				print "mergeEndCol:",mergeStartRow,mergeEndRow,mergeStartCol,mergeEndCol
+			row_data.append(getMergeCellValue(mergeStartRow,mergeEndRow,mergeStartCol,mergeEndCol));
+			colIndex = mergeEndCol
+		print "colindex:",colIndex
+		colIndex=colIndex+1
+	colRows=mergeEndRow+1
+	print row_data
