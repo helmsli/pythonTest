f6dc4f8080d64bdfd0f2eee6c791093d85715e3f
helmsli <helmsli@163.com>
2017-06-07 15:58:01 +0800

    mqtt reconnection ok

--- pythonTest/mqtt-stomp/simple.py
+++ pythonTest/mqtt-stomp/simple.py
@@ -11,44 +11,60 @@ import threading
 
 
 class MqttReConnection:
-	def reConnection(self):
+	def on_reConnection(self):
 		pass
-		
-class MqttConnection(MqttReConnection):
+	def on_connected(self):
+		pass
 
+class MqttConnection(MqttReConnection):
 	def __init__(self,connListener):
 		self.connListener = connListener
 		connListener.registerReconnection(self)
 		self.myConn= None
-	def connection(self):
+		self.myConn = stomp.Connection([('localhost', 61613)], heartbeats=(4000, 4000))
+		self.myConn.set_listener('', self.connListener)
+		self.timer=None
+		'''
+	def Connection(self):
 		print("time:%s connection  +++++++++++++++++++++++++++++" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
 		self.myConn = stomp.Connection([('localhost', 61613)], heartbeats=(4000, 4000),reconnect_attempts_max=3000)
 		print self.myConn
 		self.myConn.set_listener('', self.connListener)
-		print("connection ------------")
-		
-		self.reConnection()
-	def reConnection(self):
+		print("connection ------------")		
+		return self._reConnection()
+	'''
+	def on_connected(self):
+		try:
+			if ( self.timer is not None) and  (not self.timer.finished.is_set()):
+				self.timer.cancel()
+				self.timer=None
+		except:
+			print("on connected:")
+	def on_reConnection(self,forceStart=False):
 		#check the old connection,if old is ok,close the old connection
 		try:
 			if self.myConn is not None:
 				print("disconnction is not None")
-				self.myConn.disconnect()
-		except Exception:
-			print Exception 
-		
-	def reConnection(self):
-		try:
-			if self.myConn is not None:
-				print("disconnction is not None")
 				#self.myConn.disconnect()
-		except Exception:
-			print Exception 
+		except:
+			print("time:%s Reconnection disconnection is Error"%datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+		#start the timer connection
+		#cancel the older timer
+		if ( self.timer is not None) and  (not self.timer.finished.is_set()):
+			self.timer.cancel()
+			print("timer is cancel")
+		print("timer is start")
+		self.timer = threading.Timer(3, self._ReConnection)
+		self.timer.start()
+		print("timer start end")
+		
+	def _ReConnection(self):
 		
 		try:
+			self.timer = threading.Timer(15, self._ReConnection)
+			self.timer.start()		
 			print("time:%s reconnection "%datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
-			#self.myConn = stomp.Connection([('localhost', 61613)], heartbeats=(4000, 4000))
-			#self.myConn.set_listener('', self.connListener)
+			
 			self.myConn.start()
 			self.myConn.connect('guest', 'guest', wait=True)
 			self.myConn.subscribe(destination='/queue/test', id=1, ack='auto')
@@ -59,15 +75,13 @@ class MqttConnection(MqttReConnection):
 			print "ConnectFailedException((((((((((((((((((((((((((((((((((((((((((((((((("
 			print e
 			print traceback.format_exc()
-			timer = threading.Timer(5, self.connection)
-			timer.start()
+			
 			#continue
 		except exception.NotConnectedException as e:
 			print "NotConnectedException((((((((((((((((((((((((((((((((((((((((((((((((("
 			print e
 			print traceback.format_exc()
-			timer = threading.Timer(5, self.connection)
-			timer.start()
+			
 			#continue
 		except Exception as e:
 			print e
@@ -79,8 +93,6 @@ class MqttConnection(MqttReConnection):
 class MyListener(stomp.ConnectionListener):
 	def registerReconnection(self,mqttReconnection):
 		self.mqttReconnection = mqttReconnection
-
-
 
 	def on_message(self, headers, message):
 		print('received a message "%s"' % message)
@@ -95,20 +107,23 @@ class MyListener(stomp.ConnectionListener):
 			time.sleep(3)
 			print("disconnected exception ##########################");
 			
+	def on_connected(self, headers, body):
+		self.mqttReconnection.on_connected()
+		print("time:%s on_connected success "%datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
 	def on_disconnected(self):
 		try:
 			print("time:%s on_disconnected"%datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
-			self.mqttReconnection.reConnection()
+			self.mqttReconnection.on_reConnection()
 			print('time:%s on_disconnected_disconnected *********************************************'%datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))			
 		except Exception:
-			time.sleep(3)
+			
 			print("disconnected exception ##########################");
 			
 
 
 myListener = MyListener()
 myMqttConnection = MqttConnection(myListener)
-myMqttConnection.connection()
+myMqttConnection.on_reConnection()
 while(True):
 	command = raw_input('>>')
 	
---
+++ pythonTest/mqtt-stomp/timer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from threading import Timer  
+import time  
+  
+timer_interval=1  
+def delayrun():  
+	print 'running'
+	t=Timer(timer_interval,delayrun)  
+	t.start()  
+  
+t=Timer(timer_interval,delayrun)  
+t.start()  
+while True:  
+
+    time.sleep(10)  
+    print 'main running'  
